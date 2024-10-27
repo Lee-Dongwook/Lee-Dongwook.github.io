a29fa41679eafbaed9509c178d831456c2bf7b0d
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-27 17:30:58 +0900

    feat: Pagination

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -133,7 +133,7 @@ export default function Main() {
             <a tw="hover:text-blue-500" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
               CC BY-NC-SA 4.0
             </a>
-            <span tw="ml-2">2016-present © varHarrie</span>
+            <span tw="ml-2">2024-present © DongWook Lee</span>
           </FooterCenter>
         </Footer>
       </Wrapper>
